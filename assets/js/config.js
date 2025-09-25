ff5d492ca164adb0f94051efcf573cb927974413
gradform <gradform@outlook.com>
2025-09-25 21:51:03 +0300

    Booking Upgrades V10.3

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
   WEB_APP_URL: "https://script.google.com/macros/s/AKfycby57MjQuaen_JAa8neRr-GX89m--V2pKblin1txbKaz1_xFuowP1OcqX8QCoWHCIYVr/exec",
   RECAPTCHA_SITE_KEY: "6LfGh8ArAAAAAPaLcU1FX-nE_nrufknfUfA49LEq",
-  RENDER_URL: "https://wafa-dental-backend.onrender.com/send-confirmation"
+  RENDER_URL: "https://wafa-dental-backend.onrender.com/send-confirmation",
+  BOOKING_URL: "https://wafa-dental-backend.onrender.com/book-appointment"
 };
