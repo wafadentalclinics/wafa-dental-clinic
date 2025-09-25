55c12c2ea7381e4dadc22ebf4e5380ec8378fc1e
gradform <gradform@outlook.com>
2025-09-25 20:32:42 +0300

    Booking Upgrades V9.5

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwOV6cduYRxSkp7T8KcBQBSU07w9BZPs3DQPnekNP9rriVynAsABCuy8uDEdQgGZYGM/exec",
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzLLnptnv5P6ua8GlVpbhNYWb08GvDuIbKCyP5sKMQeiXJn2JBLV0euwgFWLaH5fGk/exec",
   RECAPTCHA_SITE_KEY: "6LfGh8ArAAAAAPaLcU1FX-nE_nrufknfUfA49LEq",
   RENDER_URL: "https://wafa-dental-backend.onrender.com/send-confirmation"
 };
